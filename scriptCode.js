c43b8c9d452cd86759844704973251f53994f7c4
yngGooby <diegoperez.a323@gmail.com>
2026-03-09 00:54:08 -0400

    this is the base code, got the sizing of the canvas down with grid, attempted to use flexbox but had various issues if i wanted to achieve the 1-100 per sides for the canvas (100x100) issues:     - grid would only go from increments of 8 16 32 64     - anything inbetween those numbers would space the grid out far and wide     - had an issue with one column missing which made the even grid layout not work     - if i made something 16x16 or any size then i would not get that exact layout         - would instead get 13x15 though i had to go against what it said it was for the best to make it more responsive!
    new issue is now wanting to be able to click a div and its able to change the background color
    then if user is still holding down the mouse button they can drag and color from anywhere in the canvas
    basically want to have more control

--- scriptCode.js
+++ scriptCode.js
@@ -1,12 +1,24 @@
 /*
 let gridArea = 16; //determines how many divs will be per side
 */
+
+
 const container = document.getElementById("container"); //calls the container div in html
 function createCells(num){ //function makes the grid based off gridArea
     for(i = 0; i < num*num; i++){
-        const divCell = document.createElement("div"); //creates the cell
-        divCell.className = "cell"; //class is now "cell" so we can adjust in css
-        container.appendChild(divCell);//makes the cells appear in the browser
+        let cellSize = Math.floor(960 / num);
+
+        const canvasCell = document.createElement("div"); //creates the cell
+        canvasCell.className = "cell"; //class is now "cell" so we can adjust in css
+        container.appendChild(canvasCell);//makes the cells appear in the browser
+        
+        canvasCell.style.width = `${cellSize}px`;
+        canvasCell.style.height = `${cellSize}px`;
+
+        canvasCell.addEventListener("mouseenter", () => {
+            canvasCell.style.backgroundColor = "red";
+        })
     }
+    container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
 }
-createCells(32)
+createCells(64)
